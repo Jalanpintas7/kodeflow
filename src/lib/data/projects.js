1e5ed81b9d951bbdc812d176d1b20df614c5d582
farhan_alfaraby <farhansakri@gmail.com>
2025-09-24 12:07:50 +0700

    fix layout

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -2,6 +2,7 @@
 import dashboardAdmin from '$lib/assets/project-images/dashboard-rayhar.png';
 import formBooking from '$lib/assets/project-images/form-booking.png';
 import sosialMediaManager from '$lib/assets/project-images/sosial-media.png';
+import foodReview from '$lib/assets/project-images/food-review.png';
 import ljss from '$lib/assets/project-images/ljss.png';
 import logo from '$lib/assets/logo.png';
 import favicon from '$lib/assets/favicon.png';
@@ -47,7 +48,8 @@ export const projects = [
     heroGradient: 'linear-gradient(135deg, #ffecd2 0%, #fcb69f 100%)',
     coverImage: sosialMediaManager,
     website: 'https://prototype-ui.netlify.app/',
-  },  {
+  },
+    {
     slug: 'system-akuntansi-trucking-kapal',
     title: 'System Akuntansi Trucking Kapal',
     excerpt: 'System Akuntansi Trucking Kapal untuk manajemen order',
@@ -60,6 +62,20 @@ export const projects = [
     heroGradient: 'linear-gradient(135deg, #ffecd2 0%, #fcb69f 100%)',
     coverImage: ljss,
     website: 'https://ljss-test.netlify.app/',
+  },
+    {
+    slug: 'food-review',
+    title: 'Food Review',
+    excerpt: 'Website untuk review makanan dan coffee shop populer di malaysia',
+    description:
+      'Sebuah website yang berfungsi untuk memberikan review terhadap berbagai macam makanan, minuman, cafe, event, dan resep masakan yang terkenal dan popular',
+    tags: ['SvelteKit', 'Supabase', 'Tailwind'],
+    whatWeDo: ['Frontend Development', 'Backend Development'],
+    deliverables: ['Landing Page'],
+    projectTimeline: '2025',
+    heroGradient: 'linear-gradient(135deg, #ffecd2 0%, #fcb69f 100%)',
+    coverImage: foodReview,
+    website: 'https://kelantan.netlify.app/',
   },
 ];
 
